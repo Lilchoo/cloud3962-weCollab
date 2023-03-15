556d42f01ddaf3e7a9582bb9cf2a3b398556af68
AlexanderLiu <alexanderliu2016@gmail.com>
2023-03-14 17:23:28 -0700

    update login layout: remove amazon logo change margins change some text

--- src/Login.js
+++ src/Login.js
@@ -35,14 +35,11 @@ function Login() {
 
     return (
         <div className='login'>
-            <Link to='/'>
-                <img
-                    className="login__logo"
-                    src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png' 
-                />
+            <Link to='/' className='no-underline text-black login-direct-home'>
+                <h1>weCollab</h1>
             </Link>
 
-            <div className='login__container'>
+            <div className='login-container'>
                 <h1>Sign-in</h1>
 
                 <form>
@@ -52,15 +49,15 @@ function Login() {
                     <h5>Password</h5>
                     <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
 
-                    <button type='submit' onClick={signIn} className='login__signInButton'>Sign In</button>
+                    <button type='submit' onClick={signIn} className='login-signInButton'>Sign In</button>
                 </form>
 
                 <p>
-                    By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please
+                    By signing-in you agree to the weCollab Conditions of Use & Sale. Please
                     see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
                 </p>
 
-                <button onClick={register} className='login__registerButton'>Create your Amazon Account</button>
+                <button onClick={register} className='login-registerButton'>Create your weCollab Account</button>
             </div>
         </div>
     )
