a45a8766941ced988e12ecebb3a6e3d03a0dded9
AlexanderLiu <alexanderliu2016@gmail.com>
2023-03-14 17:51:15 -0700

    modify layout of basket split into header body and footer section

--- src/Checkout.js
+++ src/Checkout.js
@@ -9,17 +9,12 @@ function Checkout() {
 
   return (
     <div className="checkout">
-      <div className="checkout__left">
-        <img
-          className="checkout__ad"
-          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
-          alt=""
-        />
-
-        <div>
+        <div className="checkout-header">
           <h3>Hello, {user?.email}</h3>
-          <h2 className="checkout__title">Your shopping Basket</h2>
+          <h3 className="checkout-title">Your shopping Basket</h3>
+        </div>
 
+        <div className="checkout-body">
           {basket.map(item => (
             <CheckoutProduct
               id={item.id}
@@ -29,11 +24,9 @@ function Checkout() {
               rating={item.rating}
             />
           ))}
-
         </div>
-      </div>
 
-      <div className="checkout__right">
+      <div>
         <Subtotal />
       </div>
     </div>
