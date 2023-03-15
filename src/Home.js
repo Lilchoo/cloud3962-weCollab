2803ff398c69400558431f6eb634408d50f90da0
AlexanderLiu <alexanderliu2016@gmail.com>
2023-03-15 13:20:55 -0700

    fix rating

--- src/Home.js
+++ src/Home.js
@@ -9,42 +9,42 @@ function Home() {
       id:"1",
       title: "Sneaker",
       price: '11.96',
-      rating: '🌟🌟🌟🌟🌟',
+      rating: 5,
       image: 'product1_sneaker.jpg'
     },
     {
       id:"2",
       title:"Cloth",
       price:`239.0`,
-      rating:`4`,
+      rating: 4,
       image:"product2_clothing.jpg"
     },
     {
       id:"3",
       title:"Gogle",
       price:`199.99`,
-      rating:`3`,
+      rating:3,
       image:"product3_goggle.jpg"
     },
     {
       id:"4",
       title:"Overalls",
       price:`98.99`,
-      rating:`5`,
+      rating: 5,
       image:"product4_overall.jpg"
     },
     {
       id:"5",
       title:"Watch",
       price:`598.99`,
-      rating:`4`,
+      rating: 4,
       image:"product5_watch.jpg"
     },
     {
       id:"6",
       title:"Headphones",
       price:`1094.98`,
-      rating:`4`,
+      rating: 4,
       image:"product6_headphones.jpg"
     }
 
