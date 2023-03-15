af1098964e3a5f6b0b78eeb3d9651c7e89bb877d
AlexanderLiu <alexanderliu2016@gmail.com>
2023-03-14 20:30:05 -0700

    add amount prop to

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './CheckoutProduct.css'
 import { useStateValue } from "./StateProvider";
 
-function CheckoutProduct({ id, image, title, price, rating, hideButton }) {
+function CheckoutProduct({ id, image, title, price, rating, hideButton, amount }) {
     const [{ basket }, dispatch] = useStateValue();
 
     const removeFromBasket = () => {
@@ -34,7 +34,10 @@ function CheckoutProduct({ id, image, title, price, rating, hideButton }) {
                 </div>
                 
                 {!hideButton && (
-                    <button onClick={removeFromBasket} className='checkout-remove'>Remove from Basket</button>
+                    <div className='checkout-product-remove-container'>
+                        <button onClick={removeFromBasket} className='checkout-product-remove'>Remove from Basket</button>
+                        <p className='checkout-product-counter'>{amount > 1 ? `x${amount}` : ''}</p>
+                    </div>
                 )}
             </div>
         </div>
