54bbc2111face1c6447994a0f52d70016826ac0a
Gareth Ng <ngys.gareth@gmail.com>
2023-03-15 13:05:11 -0700

    make home.js code more scalable, however rating is not working anymore

--- src/Home.js
+++ src/Home.js
@@ -3,67 +3,66 @@ import "./Home.css";
 import Product from "./Product";
 
 function Home() {
+
+    const product = [
+    {
+      id:"1",
+      title: "Sneaker",
+      price: '11.96',
+      rating: '🌟🌟🌟🌟🌟',
+      image: 'product1_sneaker.jpg'
+    },
+    {
+      id:"2",
+      title:"Cloth",
+      price:`239.0`,
+      rating:`4`,
+      image:"product2_clothing.jpg"
+    },
+    {
+      id:"3",
+      title:"Gogle",
+      price:`199.99`,
+      rating:`3`,
+      image:"product3_goggle.jpg"
+    },
+    {
+      id:"4",
+      title:"Overalls",
+      price:`98.99`,
+      rating:`5`,
+      image:"product4_overall.jpg"
+    },
+    {
+      id:"5",
+      title:"Watch",
+      price:`598.99`,
+      rating:`4`,
+      image:"product5_watch.jpg"
+    },
+    {
+      id:"6",
+      title:"Headphones",
+      price:`1094.98`,
+      rating:`4`,
+      image:"product6_headphones.jpg"
+    }
+
+  ]
   return (
     <div className="home">
       <div className="home-container">
-
-      {/* <img
-          className="home__image"
-          src="clothing_front.jpg"
-          alt=""
-          style={{ width:'8   0%', height: '600px', display: 'block', margin: '0 auto'}}
-        /> */}
-        <div className="home-row">
-          <Product
-            id="12321341"
-            title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
-            price={11.96}
-            rating={5}
-            image="product1_sneaker.jpg"
-          />
-          <Product
-            id="49538094"
-            title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
-            price={239.0}
-            rating={4}
-            image="product2_clothing.jpg"
-            style={{height: '300px'}}
-          />
-        </div>
-
-        <div className="home-row">
-          <Product
-            id="4903850"
-            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
-            price={199.99}
-            rating={3}
-            image="product3_goggle.jpg"
-          />
-          <Product
-            id="23445930"
-            title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
-            price={98.99}
-            rating={5}
-            image="product4_overall.jpg"
-          />
-          <Product
-            id="3254354345"
-            title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
-            price={598.99}
-            rating={4}
-            image="product5_watch.jpg"
-          />
-        </div>
-
-        <div className="home-row">
-          <Product
-            id="90829332"
-            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
-            price={1094.98}
-            rating={4}
-            image="product6_headphones.jpg"
-          />
-        </div>
+        {product.map(data=> (
+          <div className="home-row">
+            <Product
+              id= {data.id}
+              title= {data.title}
+              price= {data.price}
+              rating={data.rating}
+              image= {data.image}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
