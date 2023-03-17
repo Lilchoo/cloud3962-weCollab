a160d1922bb5eff5da85ebbb20ef79e59cdbe99e
Benjamin Chen <bchen109@my.bcit.ca>
2023-03-16 23:04:54 -0700

    Add AWS Amplify and Incognito

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Checkout from "./Checkout";
-import Login from "./Login";
 import Payment from "./Payment";
 import Orders from "./Orders";
 import { useStateValue } from "./StateProvider";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
+
+// AWS Amplify logic
+import { Amplify } from 'aws-amplify'
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports)
 
 const promise = loadStripe(
   "pk_test_51HPvU9DFg5koCdLGJJbNo60QAU99BejacsvnKvT8xnCu1wFLCuQP3WBArscK3RvSQmSIB3N0Pbsc7TtbQiJ1vaOi00X9sIbazL"
@@ -44,54 +51,48 @@ function App() {
 
 
   return (
-<Router>
-  <div className="app">
-    <Routes>
-      <Route path="/orders" 
-      element={
-        <>
-          <Header/>
-          <Orders/>
-        </>
-      }/>
+    <Authenticator>
+        <Router>
+          <div className="app">
+            <Routes>
+              <Route path="/orders"
+                element={
+                  <>
+                    <Header />
+                    <Orders />
+                  </>
+                } />
+              <Route path="/checkout"
+                element={
+                  <>
+                    <Header />
+                    <Checkout />
+                  </>
+                } />
 
-      <Route path="/login"
-      element={
-        <>
-          <Login/>
-        </>
-      }/>
+              <Route path="/payment"
+                element={
+                  <>
+                    <Header />
+                    <Elements stripe={promise}>
+                      <Payment />
+                    </Elements>
+                  </>
+                } />
 
-      <Route path="/checkout"
-      element={
-        <>
-          <Header />
-          <Checkout />
-        </>
-      }/>
+              <Route path="/"
+                element={
+                  <>
+                    <Header />
+                    <Home />
+                  </>
+                } />
 
-      <Route path="/payment"
-      element={
-        <>
-          <Header />
-          <Elements stripe={promise}>
-            <Payment />
-          </Elements>
-        </>
-      }/>
-      
-      <Route path="/"
-      element={
-        <>
-          <Header />
-          <Home />
-        </>
-      }/>
-
-    </Routes>
-  </div>
-</Router>
+            </Routes>
+          </div>
+        </Router>
+    </Authenticator>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
