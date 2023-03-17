cef9bdefe8d7c7199ec71f6fc83135b109909757
AlexanderLiu <alexanderliu2016@gmail.com>
2023-03-16 21:17:47 -0700

    update to react v18 render

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
+import * as ReactDOMClient from 'react-dom/client'
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import reducer, { initialState } from "./reducer";
 import { StateProvider } from "./StateProvider";
 
-ReactDOM.render(
-    <StateProvider initialState={initialState} reducer={reducer}>
-      <App />
-    </StateProvider>,
-  document.getElementById("root")
-);
+const root = ReactDOMClient.createRoot(document.getElementById("root"))
+root.render(
+  <StateProvider initialState={initialState} reducer={reducer}>
+    <App />
+  </StateProvider>,
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
