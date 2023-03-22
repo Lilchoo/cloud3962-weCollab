7aceb405f8767f837ce66f179b76239c04f3063c
Benjamin Chen <benjamin.js.chen@gmail.com>
2023-03-22 12:15:26 -0700

    updated userlambda

--- amplify/backend/function/userLambda/src/app.js
+++ amplify/backend/function/userLambda/src/app.js
@@ -24,7 +24,7 @@ if (process.env.ENV && process.env.ENV !== "NONE") {
 }
 
 const userIdPresent = false; // TODO: update in case is required to use that definition
-const partitionKeyName = "username";
+const partitionKeyName = "email";
 const partitionKeyType = "S";
 const sortKeyName = "";
 const sortKeyType = "";
